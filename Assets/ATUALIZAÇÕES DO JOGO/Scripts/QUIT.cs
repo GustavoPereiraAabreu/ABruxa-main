143c3dbe490da12d9323ce6ea3a0996c121ca7b8
GustavoPereiraAabreu <gustavop.4breu@gmail.com>
2025-09-26 10:55:52 -0300

    fff

--- Assets/ATUALIZAÇÕES DO JOGO/Scripts/QUIT.cs
+++ Assets/ATUALIZAÇÕES DO JOGO/Scripts/QUIT.cs
@@ -6,7 +6,7 @@ public class QUIT : MonoBehaviour
     public void Quit()
     {
 
-        SceneManager.LoadScene("Melissa Iniciar");
+        SceneManager.LoadScene("Melissa (Tela Inicialr");
         Time.timeScale = 1f;
     }
 }
